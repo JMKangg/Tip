ded4fffbdd6d6937820642a9f0cd995a895f2f6f
JMKangg <ajdwl0315@gmail.com>
2021-11-30 10:36:23 +0900

    jimmys Tip Calculater

--- app/src/main/java/jimmys/tip/calculator/MainActivity.kt
+++ app/src/main/java/jimmys/tip/calculator/MainActivity.kt
@@ -2,10 +2,36 @@ package jimmys.tip.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jimmys.tip.calculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateBtn.setOnClickListener{ calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOption.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_precent -> 0.20
+            R.id.option_eighteen_precent -> 0.18
+            R.id.option_fifthteen_precent -> 0.15
+            else -> 0
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip) }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
