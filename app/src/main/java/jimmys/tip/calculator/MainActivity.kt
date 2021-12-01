be38e74850b1fce7820802d5b4908cb86e023e8e
JMKangg <ajdwl0315@gmail.com>
2021-12-01 23:06:55 +0900

    jimmys Tip Calculater(final)

--- app/src/main/java/jimmys/tip/calculator/MainActivity.kt
+++ app/src/main/java/jimmys/tip/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package jimmys.tip.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import jimmys.tip.calculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,24 +15,47 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateBtn.setOnClickListener{ calculateTip() }
+        binding.calculateBtn.setOnClickListener { calculateTip() }
     }
 
-    private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOption.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
-            R.id.option_twenty_precent -> 0.20
-            R.id.option_eighteen_precent -> 0.18
-            R.id.option_fifthteen_precent -> 0.15
-            else -> 0
+    fun calculateTip() {
+        val costOfServiceValue = binding.costOfService.text
+
+        if (costOfServiceValue.isNullOrBlank()) {
+            binding.costOfService.error = "Please enter value!"
+        } else {
+
+            val selectedId = binding.tipOption.checkedRadioButtonId
+
+            val userEnterPer = binding.userWantPercent.text
+
+            if (selectedId == R.id.option_user_set) {
+                if (userEnterPer.isNullOrBlank()) {
+                    binding.userWantPercent.error = "Please enter value!"
+                    return
+                }
+            }
+
+            val tipPercentage = when (selectedId) {
+                R.id.option_twenty_precent -> 0.20
+                R.id.option_eighteen_precent -> 0.18
+                R.id.option_fifthteen_precent -> 0.15
+                R.id.option_user_set -> userEnterPer.toString().toDouble() / 100
+                else -> {
+                    Toast.makeText(this, "Please select service option below.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+
+            var tip = tipPercentage * costOfServiceValue.toString().toDouble()
+            val roundUp = binding.roundUpSwitch.isChecked
+            if (roundUp) {
+                tip = kotlin.math.ceil(tip)
+            }
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
         }
-        var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp) {
-            tip = kotlin.math.ceil(tip) }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
